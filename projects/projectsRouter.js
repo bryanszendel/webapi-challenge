85eb065fd7f78714773ce99cccc2039b74dccf08
Bryan Szendel <bryan.szendel@gmail.com>
2019-09-05 14:33:32 -0600

    basic structure of actionsRouter and productsRouter is working

---
+++ projects/projectsRouter.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const actions = require('../data/helpers/projectModel')
+
+const router = express.Router()
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express')
 
+const actionsRouter = require('./actions/actionsRouter.js')
+const projectsRouter = require('./projects/projectsRouter.js')
+
 const server = express()
+
+server.use(express.json())
+
+server.use('/actions', actionsRouter)
+server.use('/projects', projectsRouter)
+
+server.get('/', (req, res) => {
+  res.send(`<h2>API is UP</h2>`)
+})
 
 module.exports = server;
